5bfdf42e9ba828f734e1c4dd70a974f9a674d55a
qoudri4re <qoudri4re@gmail.com>
2022-10-01 03:16:18 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import Home from "./components/home/Home";
 import "./app.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./components/auth/login/Login";
-import Signup from "./components/auth/signup/Signup";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Login from "./components/auth/login/Login";
+// import Signup from "./components/auth/signup/Signup";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/signup" element={<Signup />} />
-          <Route exact path="/" element={<Home />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <Home />
+    </div>
   );
 }
 
