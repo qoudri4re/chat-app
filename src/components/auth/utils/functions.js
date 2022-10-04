d15d763ab1d621575a8f6e7db0ce398e1ae0e7c2
qoudri4re <qoudri4re@gmail.com>
2022-10-04 12:29:52 +0100

    following the DRY principle common codes are here now in functions

--- src/components/auth/utils/functions.js
+++ src/components/auth/utils/functions.js
@@ -1,6 +1,11 @@
 import { v4 as uuidv4 } from "uuid";
 
-//sets error and filters out redundant error message
+/**
+ * sets error and filters out redundant error messages
+ * @param {string} errorType
+ * @param {string} errorMessage
+ * @param {setState} setErrors
+ */
 const setErrorAndFilter = (errorType, errorMessage, setErrors) => {
   setErrors((prevVal) => {
     //filter out redundant error message
@@ -18,4 +23,40 @@ const setErrorAndFilter = (errorType, errorMessage, setErrors) => {
   });
 };
 
-export { setErrorAndFilter };
+/**
+ * saves user details {username, id, auth token} to local storage
+ * @param {*} userDetails -> the details to save
+ * @param {*} timeToLive -> expiry time
+ */
+function saveUserDetailsToLocalStorage(userDetails, timeToLive) {
+  const itemToSave = {
+    ...userDetails,
+    expiry: new Date().getTime() + timeToLive,
+  };
+  localStorage.setItem("userDetails", JSON.stringify(itemToSave));
+}
+
+function retrieveUserDetailsFromLocalStorage() {
+  const userDetailsStr = localStorage.getItem("userDetails");
+
+  //if the item dosen't exist
+  if (!userDetailsStr) {
+    return null;
+  }
+
+  const userDetails = JSON.parse(userDetailsStr);
+  const now = new Date();
+
+  if (now.getTime() >= userDetails.expiry) {
+    //details has expired
+    localStorage.removeItem("userDetails");
+    return null;
+  }
+  return userDetails;
+}
+
+export {
+  setErrorAndFilter,
+  saveUserDetailsToLocalStorage,
+  retrieveUserDetailsFromLocalStorage,
+};
