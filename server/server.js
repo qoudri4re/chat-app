82ac1a171b341a64d842826f0b97edf3992587d8
qoudri4re <qoudri4re@gmail.com>
2022-10-05 01:50:46 +0100

    added live api url to allowed cors

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://chat-app-api-ogbx.onrender.com"],
   })
 );
 
