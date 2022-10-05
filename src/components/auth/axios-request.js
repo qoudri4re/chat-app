5975c240a1cd66abeee2aacde5430814684864e6
qoudri4re <qoudri4re@gmail.com>
2022-10-05 12:31:35 +0100

    new api url

--- src/components/auth/axios-request.js
+++ src/components/auth/axios-request.js
@@ -1,8 +1,12 @@
 import axios from "axios";
+//live urls
+// 1- https://chat-app-api.up.railway.app/users fastest
+// 2 - https://chat-app-api-ogbx.onrender.com/users slow
+
+// BaseUrl = "http://localhost:3001/users";
 
 const client = axios.create({
-  baseURL: "https://chat-app-api-ogbx.onrender.com/users",
-  // baseURL: "http://localhost:3001/users",
+  baseURL: "https://chat-app-api.up.railway.app/users",
 });
 
 export default client;
