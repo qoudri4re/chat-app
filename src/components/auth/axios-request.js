ac62c43cd7a14e13829f7e4091dc6b5e3fff7795
qoudri4re <qoudri4re@gmail.com>
2022-10-05 01:47:06 +0100

    base url points to live api

--- src/components/auth/axios-request.js
+++ src/components/auth/axios-request.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://localhost:3001/users",
+  baseURL: "https://chat-app-api-ogbx.onrender.com/users",
+  // baseURL: "http://localhost:3001/users",
 });
 
 export default client;
