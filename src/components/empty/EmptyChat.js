34c20f3262e8e60d791c02a5aed7a7879893714b
qoudri4re <qoudri4re@gmail.com>
2022-10-07 12:42:21 +0100

    instead of displaying blank screen, displays a component with a message

---
+++ src/components/empty/EmptyChat.js
@@ -0,0 +1,17 @@
+import React from "react";
+import emptyChat from "./images/emptyChat.png";
+import "./style.css";
+
+function EmptyChat() {
+  return (
+    <div className="empty-container">
+      <div className="content">
+        <img src={emptyChat} alt="" />
+        <span>You have no friends</span>
+        <button>Search for friends</button>
+      </div>
+    </div>
+  );
+}
+
+export default EmptyChat;
--- src/components/home/sidebar/chatlist/ChatList.js
+++ src/components/home/sidebar/chatlist/ChatList.js
@@ -1,21 +1,25 @@
 import React from "react";
 import ChatListItem from "./ChatListItem";
 import "./chatList.css";
-
+import EmptyChat from "../../../empty/EmptyChat";
 function ChatList({ handleChatClick, friendsDetails }) {
-  let ChatListItems = friendsDetails.map((item) => {
-    return (
-      <ChatListItem
-        key={item._id}
-        id={item._id}
-        username={item.username}
-        lastMessage={"last message"}
-        unreadMessageCount={5}
-        handleChatClick={handleChatClick}
-      />
-    );
-  });
-  return <div className="chatlist">{ChatListItems}</div>;
+  if (friendsDetails.length === 0) {
+    return <EmptyChat />;
+  } else {
+    let ChatListItems = friendsDetails.map((item) => {
+      return (
+        <ChatListItem
+          key={item._id}
+          id={item._id}
+          username={item.username}
+          lastMessage={"last message"}
+          unreadMessageCount={5}
+          handleChatClick={handleChatClick}
+        />
+      );
+    });
+    return <div className="chatlist">{ChatListItems}</div>;
+  }
 }
 
 export default ChatList;
